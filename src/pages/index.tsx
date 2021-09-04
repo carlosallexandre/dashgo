c4b0f192282934ff45c9ce96136ca8c08daf63ce
carlosallexandre <carlos_costarodrigues@outlook.com>
2021-09-04 18:28:39 -0300

    feat: add singin page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,37 @@
-export default function Home() {
-  return <h1>DashGO</h1>
+import { Button, Flex, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
+
+export default function SignIn() {
+  return (
+    <Flex
+      width='100vw'
+      height='100vh'
+      alignItems='center'
+      justifyContent='center'
+    >
+      <Flex
+        as='form'
+        width='100%'
+        maxWidth='360px'
+        backgroundColor='gray.800'
+        padding='8'
+        borderRadius='8'
+        flexDirection='column'
+      >
+        <Stack spacing='4'>
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Password" />
+        </Stack>
+        
+        <Button 
+          type='submit' 
+          marginTop='8' 
+          colorScheme='pink'
+          size='lg'
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  )
 }
