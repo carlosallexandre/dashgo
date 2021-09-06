7fb456475523c0d7191913570eb66dd1573dd53c
carlosallexandre <carlosallexandre@pop-os.localdomain>
2021-09-06 18:54:00 -0300

    chore: add responsiveness to header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,23 @@
 import { Flex, Text, Box, Avatar } from '@chakra-ui/react'
 
-export function Profile() {
+interface ProfileProps {
+  isShowProfileData?: boolean;
+}
+
+export function Profile({ isShowProfileData }: ProfileProps) {
   return (
     <Flex align='center'>
-      <Box marginRight='4' textAlign='right'>
-        <Text>Carlos Alexandre</Text>
-        <Text 
-          color='gray.300' 
-          fontSize='small'
-        >
-          carlosallexandre@callweb.dev
-        </Text>
-      </Box>
+      {isShowProfileData && (
+        <Box marginRight='4' textAlign='right'>
+          <Text>Carlos Alexandre</Text>
+          <Text 
+            color='gray.300' 
+            fontSize='small'
+          >
+            carlosallexandre@callweb.dev
+          </Text>
+        </Box>
+      )}
       <Avatar 
         size='md' 
         name='Carlos Alexandre' 
