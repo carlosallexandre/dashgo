6928fa0e7f8f5be0f2ffa672bb4a932f8c571c51
carlosallexandre <carlosallexandre@pop-os.localdomain>
2021-09-07 17:35:10 -0300

    chore: show spinner on refetch data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
 import theme from '../styles/theme'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
 import { makeServer } from '../services/mirage'
@@ -17,6 +19,8 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />  
         </SidebarDrawerProvider>
       </ChakraProvider>
+
+      <ReactQueryDevtools />
     </QueryClientProvider>
   )
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -25,16 +25,28 @@ import { Sidebar } from "../../components/Sidebar";
 import { useEffect } from 'react';
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
+  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
     
-    return data
+
+    const users = data.users.map(user => ({
+      ...user,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }))
+
+    return users
+  }, {
+    staleTime: 1000 * 5, // 5 seconds
   })
 
   const isWideVersion = useBreakpointValue({
     base: false,
-    lg: true,
+    md: true
   })
   
   return (
@@ -46,7 +58,12 @@ export default function UserList() {
 
         <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
           <Flex mb='8' justify='space-between' align='center'>
-            <Heading size='lg' fontWeight='normal'>Usuários</Heading>
+            <Heading size='lg' fontWeight='normal'>
+              Usuários
+              { !isLoading && isFetching && (
+                <Spinner size='sm' color='gray.500' ml='4' />
+              )}
+            </Heading>
 
             <Link href='/users/create' passHref>
               <Button 
@@ -80,54 +97,20 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={['4', '4', '6']} color='gray.300' w='8'>
-                      <Checkbox colorScheme='pink' />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight='bold'>Carlos Alexandre</Text>
-                        <Text fontSize='sm' color='gray.300'>carlosallexandre@callweb.dev</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 Abr 2021</Td>}
-                  </Tr>
-                  <Tr>
-                    <Td px={['4', '4', '6']} color='gray.300' w='8'>
-                      <Checkbox colorScheme='pink' />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight='bold'>Carlos Alexandre</Text>
-                        <Text fontSize='sm' color='gray.300'>carlosallexandre@callweb.dev</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 Abr 2021</Td>}
-                  </Tr>
-                  <Tr>
-                    <Td px={['4', '4', '6']} color='gray.300' w='8'>
-                      <Checkbox colorScheme='pink' />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight='bold'>Carlos Alexandre</Text>
-                        <Text fontSize='sm' color='gray.300'>carlosallexandre@callweb.dev</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 Abr 2021</Td>}
-                  </Tr>
-                  <Tr>
-                    <Td px={['4', '4', '6']} color='gray.300' w='8'>
-                      <Checkbox colorScheme='pink' />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight='bold'>Carlos Alexandre</Text>
-                        <Text fontSize='sm' color='gray.300'>carlosallexandre@callweb.dev</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 Abr 2021</Td>}
-                  </Tr>
+                  {data.map(user => (
+                    <Tr key={user.id}>
+                      <Td px={['4', '4', '6']} color='gray.300' w='8'>
+                        <Checkbox colorScheme='pink' />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight='bold'>{user.name}</Text>
+                          <Text fontSize='sm' color='gray.300'>{user.email}</Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>{user.createdAt}</Td>}
+                    </Tr>
+                  ))}
                 </Tbody>
               </Table>
 
